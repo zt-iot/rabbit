0cbf7572817b67307d16bef2ccbbf17f4c2d145f
Jun Furuse <jun.furuse@dailambda.jp>
2025-05-21 09:05:05 +0900

    Reformat mli files

--- src/printer.mli
+++ src/printer.mli
@@ -9,8 +9,11 @@ val pprint_stmts : 'a Location.located list Sig.printer (* xxx *)
 val pprint_context : Context.context Sig.printer
 val pprint_definition : Context.definition Sig.printer
 val pprint_access_policy : Context.access_policy Sig.printer
-val pprint_system :
-  ((string * Syntax.expr' Location.located) list *
-   (string * string list * 'a Location.located list *
-    (string * int * int * int))
-     list * 'b Location.located list * string) list Sig.printer
+
+val pprint_system
+  : ((string * Syntax.expr' Location.located) list
+    * (string * string list * 'a Location.located list * (string * int * int * int)) list
+    * 'b Location.located list
+    * string)
+      list
+      Sig.printer
