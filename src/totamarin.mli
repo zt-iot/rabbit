e4e7d19d7b35a4ed5a12af1045c4121861affc29
Jun Furuse <jun.furuse@dailambda.jp>
2025-05-21 19:32:15 +0900

    Adds comment to Totamarin

--- src/totamarin.mli
+++ src/totamarin.mli
@@ -1,2 +1,5 @@
-(** Translate Rabbit system to Tamarin format *)
+(** Translate Rabbit system to Tamarin format
+
+    [translate_sys context (used_idents, used_string)]
+*)
 val translate_sys : Context.system -> string list * string list -> Tamarin.tamarin
