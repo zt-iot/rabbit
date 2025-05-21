2e1066e726c09cf9001d37954fe2b28ee07d5331
Jun Furuse <jun.furuse@dailambda.jp>
2025-05-21 19:38:12 +0900

    Simplifies the errors of Postprocessing

--- src/postprocessing.mli
+++ src/postprocessing.mli
@@ -3,16 +3,15 @@
     they are processed by the verification backends (Tamarin and ProVerif). *)
 
 (** Type representing errors that can occur during post-processing *)
-type postprocessing_error =
-  | ConflictingCondition' (** Error when there are conflicting conditions in the model *)
+type error
 
 (** Exception raised when a post-processing error occurs *)
-exception Error of postprocessing_error
+exception Error of error
 
 (** Print a human-readable description of a post-processing error
     @param err The error to print
     @param ppf The formatter to print to *)
-val print_error : postprocessing_error Sig.printer
+val print_error : error Sig.printer
 
 (** Optimize a Rabbit model by:
     - Unifying variables where possible
