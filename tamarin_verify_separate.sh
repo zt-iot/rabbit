8cc9fe9e15176ed923f6b73c1097cb540ca383d2
diode-lang <github.prong264@passmail.net>
2025-06-05 17:17:29 +0900

    * Remove some old logs * Verify all lemmas of 'needham_schroeder_nofilesys.rab' separately   ** NonceSecrecyB and NonceSecrecyB_alt consume too much RAM * Add script to run verification of all lemmas of a given .rab file in a   separate `tamarin-prover` process

---
+++ tamarin_verify_separate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+if [[ -z "$1" || ! $1 == *.spthy ]]; then
+  echo "Usage: $0 TAMARIN_FILE DRY_RUN_OPTION"
+  echo "Run $0 from the Rabbit root folder project"
+  exit 0
+else
+  TAMARIN_FILE=$1
+  DRY_RUN_OPT=$2
+
+
+  lemma_names=()
+  while IFS= read -r line; do 
+    # Extract all lines that start with 'lemma'
+    if [[ $line == lemma* ]]; then 
+      set -- $line # split $line into words and assign them to $1, $2, $3, ...
+      echo "Second word: $2"
+      # We consider the second word to be a lemma name if it DOES NOT contain '[', ']' or ','
+      if [[ ! "$2" == *,* && ! "$2" == *"]"* && ! "$2" == *"["* ]]; then
+        lemma_names+=("$2") 
+      fi
+    fi
+  done < $TAMARIN_FILE
+
+  base=$(basename "$TAMARIN_FILE")
+  for lemma_name in "${lemma_names[@]}"; do
+    cmd="tamarin-prover --prove=$lemma_name $TAMARIN_FILE > ./tamarin_logs/${base}_${lemma_name}.log"
+    
+    if [[ $DRY_RUN_OPT == "--dry-run" ]]; then
+      echo "Would run: $cmd"
+    else
+      eval "$cmd" # Yes I know using eval is dangerous
+    fi
+  done
+fi
