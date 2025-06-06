82054b4ef53e821d72452a42470ed9b4259b0a1e
diode-lang <github.prong264@passmail.net>
2025-06-06 17:56:19 +0900

    Edit of test.sh to make it usable on diode-lang branch, and re-add all .spthy files to versioning

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 rm -f examples/_camserver.rab.spthy
 
-if ! dune exec src/rabbit.exe -- examples/camserver.rab -o examples/_camserver.rab.spthy --post-process --tag-transition > /dev/null 2>&1; then
+if ! dune exec bin/rabbit.exe -- examples/camserver.rab -o tamarin_models/camserver_tt_pp.spthy --post-process --tag-transition > /dev/null 2>&1; then
     exit 2
 fi
 
-if diff examples/camserver.rab.spthy examples/_camserver.rab.spthy; then
+if diff tamarin_models/reference_camserver_tt_pp.spthy tamarin_models/camserver_tt_pp.spthy; then
     echo pass
 else
     echo DIFF FOUND
