b6c2d4ce85e0cb1f84dcfdacaa2b50492c015cb5
Jun Furuse <jun.furuse@dailambda.jp>
2025-07-01 10:51:25 +0900

    Updates test.sh

--- test.sh
+++ test.sh
@@ -4,22 +4,30 @@ if ! dune exec src/rabbit.exe -- examples/camserver.rab -o examples/_camserver.s
     exit 2
 fi
 
-if diff -c examples/camserver.spthy examples/_camserver.spthy; then
-    echo pass
+if [ -f examples/camserver.spthy ]; then
+    if diff -c examples/camserver.spthy examples/_camserver.spthy; then
+        echo pass
+    else
+        echo DIFF FOUND
+        exit 2
+    fi
 else
-    echo DIFF FOUND
-    exit 2
+    cp examples/_camserver.spthy examples/camserver.spthy
 fi
 
 if ! dune exec src/rabbit.exe -- examples/camserver_param.rab -o examples/_camserver_param.spthy --post-process --tag-transition > /dev/null 2>&1; then
     exit 2
 fi
 
-if diff -c examples/camserver_param.spthy examples/_camserver_param.spthy; then
-    echo pass
+if [ -f examples/camserver_param.spthy ]; then
+    if diff -c examples/camserver_param.spthy examples/_camserver_param.spthy; then
+        echo pass
+    else
+        echo DIFF FOUND
+        exit 2
+    fi
 else
-    echo DIFF FOUND
-    exit 2
+    cp examples/_camserver_param.spthy examples/camserver_param.spthy
 fi
 
 # tamarin-prover camserver.rab.spthy --prove=
