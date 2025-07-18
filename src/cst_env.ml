9cb59397be3fe34b5247441c7e1ed47e20c73d99
diode-lang <github.prong264@passmail.net>
2025-07-18 12:49:32 +0900

    Ignore warning 26 for dune build

--- src/cst_env.ml
+++ src/cst_env.ml
@@ -9,8 +9,6 @@ type var_desc = Env.var_desc [@@deriving show]
 type named_fact_desc = Env.named_fact_desc [@@deriving show]
 
 
-
-
 type secrecy_lvl = 
   | S_Ignore (* Means: Ignore this secrecy level when type-checking *)
   | Public 
@@ -25,19 +23,25 @@ type integrity_lvl =
 
 (* core type WITHOUT polymorphic types *)
 type core_type = 
+  | TUnit 
   | TChan of core_security_type list
   | TSimple of Name.ident * core_security_type list
   | TProd of core_security_type * core_security_type
+  | Dummy     (* Just to return a value when we don't care what it is *)
+              (* This should never be returned in an actual implementation *)
 [@@deriving show]
 
 and core_security_type = core_type * (secrecy_lvl * integrity_lvl) [@@deriving show]
 
 (* core type POSSIBLY WITH polymorphic types *)
-type core_function_param = 
+type core_function_param =
+  | CFP_Unit 
   | CFP_Chan of core_security_function_param list
   | CFP_Simple of Name.ident (* name of the simple type *) * core_security_function_param list (* core security types of the associated simple type *)
   | CFP_Product of core_security_function_param * core_security_function_param
   | CFP_Poly of Name.ident
+  | CFP_Dummy (* Just to return a value when we don't care what it is *)
+              (* This should never be returned in an actual implementation *)
 [@@deriving show]
 
 (* contructing a CFP_Poly with secrecy and integrity information should be illegal, but there is 
@@ -49,12 +53,16 @@ and core_security_function_param = core_function_param * (secrecy_lvl * integrit
 let rec cst_to_csfp (cst : core_security_type) : core_security_function_param = 
   let (core_ty, security_info) = cst in
   let converted_core_function_param = match core_ty with
+    | TUnit -> CFP_Unit
     | TChan core_security_types -> 
         CFP_Chan (List.map cst_to_csfp core_security_types)
     | TSimple (name, core_security_types) -> 
         CFP_Simple (name, List.map cst_to_csfp core_security_types)
     | TProd (cst1, cst2) -> 
         CFP_Product (cst_to_csfp cst1, cst_to_csfp cst2)
+
+    | Dummy -> CFP_Dummy
+    
   in
   (converted_core_function_param, security_info)
 
@@ -65,7 +73,7 @@ type desc =
   | SimpleTypeDef of name list (* simple type declaration *)
   | Var of var_desc
   | ExtFun of core_security_function_param list (* equational theory function with 0 or more function parameters *)
-  | ExtSyscall of core_security_function_param list (** system call with 0 ore mor function parameters *)
+  | ExtSyscall of core_security_function_param list (** system call with 0 or more function parameters *)
   | MemberFunc of core_security_function_param list (** member function of a process *)
   | Const of bool (* with param or not *) * core_security_type (* conversion from Env.Const fails if type is not given *)
   | ChannelDecl of bool (* with param or not *) * ident (* channel type *)
--- src/typechecker.ml
+++ src/typechecker.ml
@@ -4,18 +4,80 @@ exception TypeException of string
 
 
 
-let typeof_expr (_ : To_cst.cst_access_policy)
-  (_ : To_cst.cst_access_policy) (expr : Cst_syntax.expr) : Cst_env.core_security_type = match expr.Cst_syntax.desc with 
-  | _ -> failwith "TODO"
+let rec typeof_expr (secrecy_lattice : To_cst.cst_access_policy)
+  (integrity_lattice : To_cst.cst_access_policy) (expr : Cst_syntax.expr) : Cst_env.core_security_type = 
+  let typeof_expr_rec = (typeof_expr secrecy_lattice integrity_lattice) in 
+  match expr.desc with
+    | Ident { id; desc; param } ->
+        (* Handle identifier *)
+        (* if (Ident.string_part id = "msg") then 
+        print_endline (Cst_syntax.show_expr expr); *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Boolean b ->
+        (* Handle boolean *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | String s ->
+        (* Handle string *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Integer i ->
+        (* Handle integer *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Float f ->
+        (* Handle float *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Apply (id, args) ->
+        let types_of_args = List.map typeof_expr_rec args in 
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Tuple exprs ->
+        (* Handle tuple *)
+        (Dummy, (S_Ignore, I_Ignore))
+
+    | Unit ->
+        (* Handle unit *)
+        (Dummy, (S_Ignore, I_Ignore))
 
 
-let typeof_cmd  (_ : To_cst.cst_access_policy)
-  (_ : To_cst.cst_access_policy) (cmd : Cst_syntax.cmd) : Cst_env.core_security_type = match cmd.Cst_syntax.desc with 
-  | _ -> failwith "TODO"
+let rec typeof_cmd  (secrecy_lattice : To_cst.cst_access_policy)
+  (integrity_lattice : To_cst.cst_access_policy) (cmd : Cst_syntax.cmd) : Cst_env.core_security_type = 
+  let typeof_expr_rec = (typeof_expr secrecy_lattice integrity_lattice) in 
+  let typeof_cmd_rec = (typeof_cmd secrecy_lattice integrity_lattice) in 
+  match cmd.Cst_syntax.desc with 
+    | Skip ->  (TUnit, (Public, Untrusted))
+    | Sequence (c1, c2) -> 
+        let _ = (typeof_cmd secrecy_lattice integrity_lattice c1) in 
+        (typeof_cmd secrecy_lattice integrity_lattice c2)
+    | Put facts -> (Dummy, (S_Ignore, I_Ignore))
+    | Let (id, e, c) -> 
+        (* if the variable we are assigning to is `msg` *)
+        let _ = (typeof_expr secrecy_lattice integrity_lattice e) in
+        (typeof_cmd secrecy_lattice integrity_lattice c)
+    | Assign (Some id, e) -> 
+        let _ = (typeof_expr secrecy_lattice integrity_lattice e) in 
+        (TUnit, (Public, Untrusted))
+    | Assign (None, e) -> 
+        print_endline (Cst_syntax.show_expr e);
+        let _ = (typeof_expr secrecy_lattice integrity_lattice e) in 
+        (TUnit, (Public, Untrusted))
+    | Case cases ->  (Dummy, (S_Ignore, I_Ignore))
+    | While (guards, untils) -> (Dummy, (S_Ignore, I_Ignore))
+    | Event facts ->  (Dummy, (S_Ignore, I_Ignore))
+    | Return e ->  (Dummy, (S_Ignore, I_Ignore))
+    | New (id, sec_ty_opt, constr_opt, body) ->  (Dummy, (S_Ignore, I_Ignore))
+    | Get (ids, e, name, body) -> (Dummy, (S_Ignore, I_Ignore))
+    | Del (e, name) ->  (Dummy, (S_Ignore, I_Ignore))
 
 
 let typecheck_decl (secrecy_lattice : To_cst.cst_access_policy)
-  (integrity_lattice : To_cst.cst_access_policy) (decl : Cst_syntax.decl) : unit = match decl.Cst_syntax.desc with 
+  (integrity_lattice : To_cst.cst_access_policy) (decl : Cst_syntax.decl) : unit = 
+  let typeof_expr_rec = (typeof_expr secrecy_lattice integrity_lattice) in 
+  let typeof_cmd_rec = (typeof_cmd secrecy_lattice integrity_lattice) in 
+  match decl.Cst_syntax.desc with 
   | Cst_syntax.Syscall _ ->
       (* failwith "TODO: not sure if Cst_syntax.Attack needs to be typechecked"; *)
       ()
@@ -25,10 +87,8 @@ let typecheck_decl (secrecy_lattice : To_cst.cst_access_policy)
       ()
 
   | Cst_syntax.Process { id; main; _ } ->
+      (print_endline (Format.sprintf "Running typeof_cmd on process name %s" (Ident.string_part id)));
       (* we should only need to typecheck the Process decl *)
-      if (Ident.string_part id = "alice") then
-        let _ = (print_endline (Cst_syntax.show_decl decl));
-      in 
       let _ = (typeof_cmd secrecy_lattice integrity_lattice main) in ()
 
   | Cst_syntax.System _ ->
@@ -43,12 +103,18 @@ let typecheck_sys (cst_decls : Cst_syntax.decl list)
 
     (* for all `proc_name` \in `procs`, we need to check that `proc_name` is well-typed *)
 
-    (* get only the declarations for which their name appears in `procs` *)
+    (* get only the declarations for which their name appears in `proc_strs` *)
     let filtered_decls =
       List.filter (fun decl -> match decl.Cst_syntax.desc with
         | Cst_syntax.Process { id; _ } -> List.mem (Ident.string_part id) proc_strs
         | _ -> false  (* keep all non-process declarations *)
       ) cst_decls in 
+
+    (print_endline "typecheck sys : printing filtered_decls process names");
+    List.iter (fun decl -> match decl.Cst_syntax.desc with 
+        | Cst_syntax.Process { id; _ } -> print_endline (Ident.string_part id) 
+        | _ -> ()
+    ) filtered_decls;
     
     (* typecheck each decl of filtered_decls *)
     List.iter (typecheck_decl
