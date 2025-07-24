9d00d7cffbb49afcaa0bb7bae0f2d21bead57e6d
Jun Furuse <jun.furuse@dailambda.jp>
2025-07-24 18:38:21 +0900

    test.sh: better messages

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 rm -f examples/_camserver.spthy
 
-if ! dune exec src/rabbit.exe -- examples/camserver.rab -o examples/_camserver.spthy --post-process --tag-transition > /dev/null 2>&1; then
+if ! dune exec src/rabbit.exe -- examples/camserver.rab -o examples/_camserver.spthy --post-process --tag-transition --svg > /dev/null 2>&1; then
     exit 2
 fi
 
 if [ -f examples/camserver.spthy ]; then
     if diff -c examples/camserver.spthy examples/_camserver.spthy; then
-        echo pass
+        echo examples/_camserver.spthy: no change
     else
         echo DIFF FOUND
         exit 2
@@ -15,13 +15,13 @@ else
     cp examples/_camserver.spthy examples/camserver.spthy
 fi
 
-if ! dune exec src/rabbit.exe -- examples/camserver_param.rab -o examples/_camserver_param.spthy --post-process --tag-transition > /dev/null 2>&1; then
+if ! dune exec src/rabbit.exe -- examples/camserver_param.rab -o examples/_camserver_param.spthy --post-process --tag-transition --svg > /dev/null 2>&1; then
     exit 2
 fi
 
 if [ -f examples/camserver_param.spthy ]; then
     if diff -c examples/camserver_param.spthy examples/_camserver_param.spthy; then
-        echo pass
+        echo examples/_camserver_param.spthy: no change
     else
         echo DIFF FOUND
         exit 2
