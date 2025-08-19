f9981c2981da853040119bef0b69b7d7dce5780c
diode-lang <github.prong264@passmail.net>
2025-08-19 19:49:33 +0900

    Successfully split Rabbit into a main and lib project, after deleting unused printer.ml file

--- main/rabbit.ml
+++ main/rabbit.ml
@@ -1,4 +1,4 @@
-(* 
+open Rabbit_lib
 
 (** Clerial main program *)
 
@@ -220,4 +220,4 @@ let _main =
       exit 1
   | Postprocessing.Error err ->
       Print.message ~loc:Location.Nowhere "Translate error" "%t" (Postprocessing.print_error err);
-      exit 1 *)
+      exit 1
