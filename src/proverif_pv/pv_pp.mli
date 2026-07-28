f48f1490f2047c3a6244af5c068a4358d97f6780
Jun Furuse <jun.furuse@dailambda.jp>
2026-07-28 18:38:09 +0900

    Add safe ProVerif pretty-print fallback

--- src/proverif_pv/pv_pp.mli
+++ src/proverif_pv/pv_pp.mli
@@ -5,3 +5,4 @@ type program = Pv_parser.program
 val pp_program : Format.formatter -> program -> unit
 val to_string : program -> string
 val to_string_with_parens : bool -> program -> string
+val to_string_safe : program -> string * (unit, unit) result
