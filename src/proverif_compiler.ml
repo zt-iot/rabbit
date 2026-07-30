9bf42cbd0eadf5747f9abcbcada152b4f8bfc964
Jun Furuse <jun.furuse@dailambda.jp>
2026-07-30 12:17:41 +0900

    Implement term literal lowering in ProVerif compiler

--- src/proverif_compiler.ml
+++ src/proverif_compiler.ml
@@ -1,5 +1,13 @@
 open Rabbit_proverif_pv_parse
 
+type env =
+  { string_table : (string, Pitptree.ident) Hashtbl.t
+  }
+
+let create_env () : env =
+  { string_table = Hashtbl.create 16
+  }
+
 let pv_ident (s : string) : Pitptree.ident = s, Parsing_helper.dummy_ext
 
 let compile_ident (id : Typed.ident) : Pitptree.ident = pv_ident (Ident.to_string id)
@@ -8,16 +16,91 @@ let bitstring_ident : Pitptree.ident = pv_ident "bitstring"
 
 let term (t : Pitptree.term) : Pitptree.term_e = t, Parsing_helper.dummy_ext
 
-let rec compile_expr_to_term (expr : Typed.expr) : Pitptree.term_e =
+let is_ident_char = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' -> true
+  | _ -> false
+
+let sanitize_string_for_ident (s : string) : string =
+  let buf = Buffer.create (String.length s) in
+  let prev_underscore = ref false in
+  String.iter
+    (fun c ->
+       if is_ident_char c then (
+         Buffer.add_char buf c;
+         prev_underscore := false
+       ) else if not !prev_underscore then (
+         Buffer.add_char buf '_';
+         prev_underscore := true
+       ))
+    s;
+  let sanitized = Buffer.contents buf in
+  let len = String.length sanitized in
+  let start =
+    let rec find i =
+      if i >= len then len
+      else if sanitized.[i] = '_' then find (i + 1)
+      else i
+    in
+    find 0
+  in
+  let stop =
+    let rec find i =
+      if i < start then start - 1
+      else if sanitized.[i] = '_' then find (i - 1)
+      else i
+    in
+    find (len - 1)
+  in
+  if start > stop then
+    "string"
+  else
+    String.sub sanitized start (stop - start + 1)
+
+let fresh_string_ident (env : env) (s : string) : Pitptree.ident =
+  match Hashtbl.find_opt env.string_table s with
+  | Some id -> id
+  | None ->
+      let base = "str__" ^ sanitize_string_for_ident s in
+      let rec loop i =
+        let name =
+          if i = 1 then base else Printf.sprintf "%s__%d" base i
+        in
+        let id = pv_ident name in
+        if Hashtbl.fold (fun _ existing found -> found || existing = id) env.string_table false then
+          loop (i + 1)
+        else
+          id
+      in
+      let id = loop 1 in
+      Hashtbl.add env.string_table s id;
+      id
+
+let zero_term () : Pitptree.term_e = term (Pitptree.PIdent (pv_ident "0"))
+
+let rec unfold_int (t : Pitptree.term_e) (n : int) : Pitptree.term_e =
+  match n with
+  | 0 -> t
+  | n ->
+      term (Pitptree.PFunApp (pv_ident "+", [unfold_int t (n - 1)]))
+
+let unfold_int_minus (t : Pitptree.term_e) (n : int) : Pitptree.term_e =
+  match n with
+  | 0 -> t
+  | n ->
+      term (Pitptree.PFunApp (pv_ident ("- " ^ string_of_int n), [t]))
+
+let rec compile_expr_to_term (env : env) (expr : Typed.expr) : Pitptree.term_e =
   match expr.desc with
   | Typed.Ident { id; _ } -> term (Pitptree.PIdent (compile_ident id))
   | Apply (id, args) ->
-      term (Pitptree.PFunApp (compile_ident id, List.map compile_expr_to_term args))
-  | Tuple exprs -> term (Pitptree.PTuple (List.map compile_expr_to_term exprs))
+      term (Pitptree.PFunApp (compile_ident id, List.map (compile_expr_to_term env) args))
+  | Tuple exprs -> term (Pitptree.PTuple (List.map (compile_expr_to_term env) exprs))
   | Unit -> term (Pitptree.PTuple [])
-  | String _ -> assert false
-  | Boolean _ -> assert false
-  | Integer _ -> assert false
+  | String s -> term (Pitptree.PIdent (fresh_string_ident env s))
+  | Boolean true -> term (Pitptree.PIdent (pv_ident "true"))
+  | Boolean false -> term (Pitptree.PIdent (pv_ident "false"))
+  | Integer n when n >= 0 -> unfold_int (zero_term ()) n
+  | Integer n -> unfold_int_minus (zero_term ()) (-n)
   | Float _ -> assert false
 
 let compile_function (id : Typed.ident) (arity : int) : Pitptree.tdecl =
@@ -25,13 +108,13 @@ let compile_function (id : Typed.ident) (arity : int) : Pitptree.tdecl =
   let arg_tys = List.init arity (fun _ -> bitstring_ident) in
   Pitptree.TFunDecl (name, arg_tys, bitstring_ident, [])
 
-let compile_equation (lhs : Typed.expr) (rhs : Typed.expr) : Pitptree.tdecl =
+let compile_equation (env : env) (lhs : Typed.expr) (rhs : Typed.expr) : Pitptree.tdecl =
   let envdecl =
     List.sort_uniq compare (Typed.vars_of_expr lhs @ Typed.vars_of_expr rhs)
     |> List.map (fun id -> compile_ident id, bitstring_ident)
   in
-  let lhs_term = compile_expr_to_term lhs in
-  let rhs_term = compile_expr_to_term rhs in
+  let lhs_term = compile_expr_to_term env lhs in
+  let rhs_term = compile_expr_to_term env rhs in
   let equality_term =
     term (Pitptree.PFunApp (pv_ident "=", [lhs_term; rhs_term]))
   in
