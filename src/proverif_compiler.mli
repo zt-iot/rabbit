4242f64260de9430569d1bb6dbfe7ceefb1a10ca
Jun Furuse <jun.furuse@dailambda.jp>
2026-07-31 00:00:19 +0900

    Add ProVerif example driver and init lowering

---
+++ src/proverif_compiler.mli
@@ -0,0 +1,14 @@
+open Rabbit_proverif_pv_parse
+
+type error =
+  | Unsupported of string
+  | Invalid_input of string
+  | Internal_error of string
+
+exception Error of error Location.located
+
+val print_error : error -> Format.formatter -> unit
+
+val compile_program :
+  Typed.decl list ->
+  Pitptree.tdecl list * Pitptree.tprocess_e * Pitptree.tprocess_e option
