0c1d2a32e40366c0a20f9934b41d8a86afaae41f
Jun Furuse <jun.furuse@dailambda.jp>
2026-07-30 14:32:03 +0900

    Implement ProVerif type and allow lowering

--- src/proverif_compiler.ml
+++ src/proverif_compiler.ml
@@ -13,10 +13,16 @@ let print_error err ppf =
 
 type env =
   { string_table : (string, Pitptree.ident) Hashtbl.t
+  ; syscall_table : (string, Pitptree.ident) Hashtbl.t
+  ; allow_entries : (Pitptree.ident * Pitptree.ident * Pitptree.ident) Queue.t
+  ; top_process : Pitptree.tprocess_e option ref
   }
 
 let create_env () : env =
   { string_table = Hashtbl.create 16
+  ; syscall_table = Hashtbl.create 16
+  ; allow_entries = Queue.create ()
+  ; top_process = ref None
   }
 
 let pv_ident (s : string) : Pitptree.ident = s, Parsing_helper.dummy_ext
@@ -35,6 +41,8 @@ let true_ident : Pitptree.ident = pv_ident "true"
 let false_ident : Pitptree.ident = pv_ident "false"
 
 let term (t : Pitptree.term) : Pitptree.term_e = t, Parsing_helper.dummy_ext
+let pterm (t : Pitptree.pterm) : Pitptree.pterm_e = t, Parsing_helper.dummy_ext
+let process (p : Pitptree.tprocess) : Pitptree.tprocess_e = p, Parsing_helper.dummy_ext
 
 let is_ident_char = function
   | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' -> true
@@ -94,6 +102,26 @@ let fresh_string_ident (env : env) (s : string) : Pitptree.ident =
       let id = loop 1 in
       Hashtbl.add env.string_table s id;
       id
+
+let fresh_syscall_ident (env : env) (id : Typed.ident) : Pitptree.ident =
+  let name = Ident.to_string id in
+  match Hashtbl.find_opt env.syscall_table name with
+  | Some id -> id
+  | None ->
+      let base = sanitize_string_for_ident name ^ "_s" in
+      let rec loop i =
+        let candidate =
+          if i = 1 then base else Printf.sprintf "%s__%d" base i
+        in
+        let id = pv_ident candidate in
+        if Hashtbl.fold (fun _ existing found -> found || existing = id) env.syscall_table false then
+          loop (i + 1)
+        else
+          id
+      in
+      let syscall_ident = loop 1 in
+      Hashtbl.add env.syscall_table name syscall_ident;
+      syscall_ident
 
 let zero_term () : Pitptree.term_e = term (Pitptree.PIdent (pv_ident "0"))
 
@@ -158,16 +186,37 @@ let compile_attack
   =
   error ~loc (Unsupported "compile_attack is not implemented yet")
 
-let compile_type ~loc (_id : Typed.ident) (_typclass : Input.type_class) =
-  error ~loc (Unsupported "compile_type is not implemented yet")
+let compile_type ~loc:(_loc : Location.t) (id : Typed.ident) (typclass : Input.type_class) =
+  let ty =
+    match typclass with
+    | CProc -> proc_t_ident
+    | CFsys | CChan -> acc_data_t_ident
+  in
+  Pitptree.TConstDecl (compile_ident id, ty, [])
 
 let compile_allow
     ~loc
-    (_process_typ : Typed.ident)
-    (_target_typs : Typed.ident list)
-    (_syscalls : Typed.ident list option)
+    (env : env)
+    (process_typ : Typed.ident)
+    (target_typs : Typed.ident list)
+    (syscalls : Typed.ident list option)
   =
-  error ~loc (Unsupported "compile_allow is not implemented yet")
+  let process_typ = compile_ident process_typ in
+  let target_typs = List.map compile_ident target_typs in
+  match syscalls with
+  | None ->
+      error ~loc
+        (Unsupported "allow ... [.] is not supported yet in ProVerif translation")
+  | Some syscalls ->
+      List.iter
+        (fun target_typ ->
+           List.iter
+             (fun syscall ->
+                let syscall_ident = fresh_syscall_ident env syscall in
+                Queue.add (process_typ, target_typ, syscall_ident) env.allow_entries)
+             syscalls)
+        target_typs;
+      []
 
 let compile_allow_attack
     ~loc
@@ -202,6 +251,7 @@ let compile_process
 
 let compile_system
     ~loc
+    (_env : env)
     (_procs : Typed.proc_group_desc list)
     (_lemmas : (Typed.ident * Typed.lemma) list)
   =
@@ -227,6 +277,27 @@ let compile_generated_string_consts (env : env) : Pitptree.tdecl list =
   |> List.sort_uniq compare
   |> List.map (fun id -> Pitptree.TConstDecl (id, bitstring_ident, []))
 
+let compile_generated_syscall_consts (env : env) : Pitptree.tdecl list =
+  Hashtbl.to_seq_values env.syscall_table
+  |> List.of_seq
+  |> List.sort_uniq compare
+  |> List.map (fun id -> Pitptree.TConstDecl (id, syscall_t_ident, []))
+
+let wrap_with_allow_init (env : env) (body : Pitptree.tprocess_e) : Pitptree.tprocess_e =
+  let entries = List.of_seq (Queue.to_seq env.allow_entries) in
+  List.fold_right
+    (fun (process_typ, target_typ, syscall_ident) acc ->
+       process
+         (PInsert
+            ( access_control_table_ident
+            , [ pterm (PPIdent process_typ)
+              ; pterm (PPIdent target_typ)
+              ; pterm (PPIdent syscall_ident)
+              ]
+            , acc )))
+    entries
+    body
+
 let rec compile_load (env : env) (_filename : string) (decls : Typed.decl list) : Pitptree.tdecl list =
   List.concat_map (compile_decl env) decls
 
@@ -238,19 +309,27 @@ and compile_decl (env : env) (decl : Typed.decl) : Pitptree.tdecl list =
   | Attack { id; syscall; args; cmd } -> [compile_attack ~loc:decl.loc id syscall args cmd]
   | Type { id; typclass } -> [compile_type ~loc:decl.loc id typclass]
   | Allow { process_typ; target_typs; syscalls } ->
-      [compile_allow ~loc:decl.loc process_typ target_typs syscalls]
+      compile_allow ~loc:decl.loc env process_typ target_typs syscalls
   | AllowAttack { process_typs; attacks } ->
       [compile_allow_attack ~loc:decl.loc process_typs attacks]
   | Init { id; desc } -> [compile_init ~loc:decl.loc id desc]
   | Channel { id; param; typ } -> [compile_channel ~loc:decl.loc id param typ]
   | Process { id; param; args; typ; files; vars; funcs; main } ->
       [compile_process ~loc:decl.loc id param args typ files vars funcs main]
-  | System (procs, lemmas) -> [compile_system ~loc:decl.loc procs lemmas]
+  | System (procs, lemmas) -> [compile_system ~loc:decl.loc env procs lemmas]
   | Load (filename, decls) -> compile_load env filename decls
 
-let compile_program (decls : Typed.decl list) : Pitptree.tdecl list =
+let compile_program (decls : Typed.decl list) =
   let env = create_env () in
   let body = List.concat_map (compile_decl env) decls in
-  compile_prelude env
-  @ compile_generated_string_consts env
-  @ body
+  let top_process =
+    match !(env.top_process) with
+    | Some top_process -> wrap_with_allow_init env top_process
+    | None -> wrap_with_allow_init env (process PNil)
+  in
+  ( compile_prelude env
+    @ compile_generated_syscall_consts env
+    @ compile_generated_string_consts env
+    @ body
+  , top_process
+  , None )
